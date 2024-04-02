15d2cee8970266477244aa8ca0748dc3fe3ded84
Efe Caylan <103333941+matrix373@users.noreply.github.com>
2024-04-02 02:45:44 +0000

    Images removed

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 // - Here we have an array of objects
 // - We might see this when we have multiple rows of results from our database or API
 const beans = [
-  { id: 1, title: 'Arabica', origin: 'Native to Ethiopia', flavor: 'Varaible-distinctly not bitter', bestuse: 'Brewed coffee', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Starr_070308-5472_Coffea_arabica.jpg/1280px-Starr_070308-5472_Coffea_arabica.jpg', isBrewedCoffe: true },
-  { id: 2, title: 'Robusta', origin: 'Native to sub-Saharan Africa', flavor: 'Bitter, woody or nutty', bestuse: 'Coffee blends and espresso', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Ripe_Seeds_Coffee_Robusta_Coorg_Karnataka_India_Feb24_D72_25688.jpg/800px-Ripe_Seeds_Coffee_Robusta_Coorg_Karnataka_India_Feb24_D72_25688.jpg', isBrewedCoffe: false  },
-  { id: 3, title: 'Excelsa', origin: 'South Asia', flavor: 'Complex, tart, fruity and dark', bestuse: 'Brewed coffee and blends', image: 'https://perfectdailygrind.com/wp-content/uploads/2021/04/Excelsa-Coffee-3.jpg', isBrewedCoffe: true },
-  { id: 4, title: 'Liberica', origin: 'South Asia', flavor: 'Unusual, nutty and woody', bestuse: 'Brewed coffee and desserts', image: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Liberica_coffee_beans%2C_roasted.jpg', isBrewedCoffe: true  },
+  { id: 1, title: 'Arabica', origin: 'Native to Ethiopia', flavor: 'Varaible-distinctly not bitter', bestuse: 'Brewed coffee', isBrewedCoffe: true },
+  { id: 2, title: 'Robusta', origin: 'Native to sub-Saharan Africa', flavor: 'Bitter, woody or nutty', bestuse: 'Coffee blends and espresso', isBrewedCoffe: false  },
+  { id: 3, title: 'Excelsa', origin: 'South Asia', flavor: 'Complex, tart, fruity and dark', bestuse: 'Brewed coffee and blends', isBrewedCoffe: true },
+  { id: 4, title: 'Liberica', origin: 'South Asia', flavor: 'Unusual, nutty and woody', bestuse: 'Brewed coffee and desserts', isBrewedCoffe: true  },
 ];
 
 
@@ -29,15 +29,6 @@ function BeanDetail({bean}) {
         <li>Flavor: {bean.flavor}</li>
         <li>Best Used By: {bean.bestuse}</li>
       </ul>
-      {bean.image && (
-        <img
-          src={bean.image}
-          alt={bean.title}
-          style={{ 
-            width: 'auto', height: 'auto'
-          }}
-        />
-      )}
     </div>
   );
 }
